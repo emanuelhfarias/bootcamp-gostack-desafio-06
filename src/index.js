4471dc9fd9f8bd05c497611358d5bdee03dda183
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-20 22:12:52 -0300

    configure StatusBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
 import 'react-native-gesture-handler';
 
 export default function App() {
     return (
-      <Routes />
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </>
     );
 }
