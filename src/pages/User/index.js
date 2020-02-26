bdd10804feb5e2f29dfea357a1fb53327db0795d
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-25 23:04:35 -0300

    navigate to User page
    yarn add prop-types
    use prop-types to map types from navigation

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+export default function User(props) {
+  console.log(props.navigation.getParam('user'));
   return <View />;
 }
